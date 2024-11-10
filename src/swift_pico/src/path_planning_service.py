771d2ce02fa89857bcebe56fb65d42c8d17f9ae5
Loki-Silvres <silvresloki@gmail.com>
2024-11-11 03:04:11 +0530

    path planing perfect, added image

--- src/swift_pico/src/path_planning_service.py
+++ src/swift_pico/src/path_planning_service.py
@@ -16,6 +16,7 @@ class WayPoints(Node):
     def __init__(self, debug = False):
         super().__init__('waypoints_service')
         self.bit_map_path = '/home/loki/pico_ws/src/swift_pico/scripts/2D_bit_map.png'
+        
 
         self.get_logger().info('Waypoints service started') 
 
@@ -24,7 +25,9 @@ class WayPoints(Node):
         self.initial_point = (500, 500)
         self.first_point = None
         self.second_point = None
-        self.scale = 0.1
+        self.step = 1
+
+        self.scale = 1
         self.debug = debug
         self.paths = []
 
@@ -54,7 +57,7 @@ class WayPoints(Node):
         for iu in range(len(self.paths)):
             path = self.paths[iu]
             for i in range(len(path)):
-                path[i] = (int(path[i][0] / self.scale), int(path[i][1] / self.scale))
+                path[i] = (int(path[i][1] / self.scale), int(path[i][0] / self.scale))
             self.paths[iu] = path
 
         self.initial_point = (int(self.initial_point[0] / self.scale), int(self.initial_point[1] / self.scale))
@@ -67,6 +70,20 @@ class WayPoints(Node):
         except:
             self.height, self.width = self.resized_img.shape
 
+    def scale_path(self, scale):
+        for iu in range(len(self.paths)):
+            path = self.paths[iu]
+            for i in range(len(path)):
+                path[i] = ((path[i][1] * scale), (path[i][0] * scale))
+            self.paths[iu] = path
+        
+    def translate_path(self):
+        for iu in range(len(self.paths)):
+            path = self.paths[iu]
+            for i in range(len(path)):
+                path[i] = (path[i][0] - self.initial_point[0], path[i][1] - self.initial_point[1])
+            self.paths[iu] = path
+
     def get_waypoints(self, msg: Int32MultiArray):
         self.first_point = (msg.data[0], msg.data[1])
         self.second_point = (msg.data[2], msg.data[3])
@@ -77,14 +94,16 @@ class WayPoints(Node):
 
         path1 = self.get_trajectory(self.initial_point, self.first_point)
         path2 = self.get_trajectory(self.first_point, self.second_point)
+
+        if path1 is None or path2 is None:
+            print("No path found between the points.")
+            return
         self.paths.append(path1)
         self.paths.append(path2)
 
-        self.de_adjust_scale()
-
-
+        print(len(self.paths))
         # Plot the path on the image
-        if self.paths:
+        if len(self.paths):
             path = []
             for i in range(len(self.paths)):
                 path += self.paths[i]
@@ -97,14 +116,24 @@ class WayPoints(Node):
                 path_x, path_y = zip(*path)
                 plt.plot(path_y, path_x, 'b-', linewidth=2, label='Path')  # Path in blue
                 plt.legend()
+                plt.savefig("/home/loki/pico_ws/src/swift_pico/scripts/path.png")
                 plt.show()
         else:
             print("No path found between the points.")
         self.destroy_subscription(self.random_points_sub)
+        self.translate_path()
+        self.scale_path(self.scale / 40)
+        # self.de_adjust_scale()
+
 
 
     def get_trajectory(self, first_point, second_point):
         resized_img = (self.resized_img == 255).astype(int)
+        resized_img_blur = cv.blur(self.resized_img, (100, 100))
+        resized_img_blur = cv.blur(resized_img_blur, (100, 100))
+        # cv.imshow("Arena", resized_img_blur)
+        # if cv.waitKey(0) & 0xFF == ord('q'):
+        #     raise KeyboardInterrupt
         rows, cols = self.height, self.width
         start_point = (first_point[1], first_point[0])
         finish_point = (second_point[1], second_point[0])
@@ -125,21 +154,31 @@ class WayPoints(Node):
                 path.append(start_point)
                 path.reverse()
                 return path
-            
-            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            for dx, dy in [(-self.step, 0), (self.step, 0), (0, -self.step), (0, self.step)]:
                 neighbor = (current[0] + dx, current[1] + dy)
                 if 0 <= neighbor[0] < rows and 0 <= neighbor[1] < cols and resized_img[neighbor[0], neighbor[1]] == 1:
                     tentative_g_score = g_score[current] + 1
                     if neighbor not in g_score or tentative_g_score < g_score[neighbor]:
                         came_from[neighbor] = current
                         g_score[neighbor] = tentative_g_score
-                        f_score[neighbor] = tentative_g_score + distance.euclidean(neighbor, finish_point)
+                        f_score[neighbor] = tentative_g_score + distance.euclidean(neighbor, finish_point) - resized_img_blur[neighbor[0], neighbor[1]]
                         heapq.heappush(open_set, (f_score[neighbor], neighbor))
-        
         return None  # No path found
         
     def waypoint_callback(self, request, response):
+        
+        if not self.paths:
+            return 
+        self.waypoints = []
+        for i in range(len(self.paths)):
+                self.waypoints += self.paths[i]
+        for i in range(len(self.waypoints)):
+            self.waypoints[i] = (float(self.waypoints[i][0]), float(self.waypoints[i][1]), 27.0)
 
+        ways = []
+        for i in range(0, len(self.waypoints), 10):
+            ways += [self.waypoints[i]]
+        self.waypoints = ways
         if request.get_waypoints == True :
             response.waypoints.poses = [Pose() for _ in range(len(self.waypoints))]
             for i in range(len(self.waypoints)):
